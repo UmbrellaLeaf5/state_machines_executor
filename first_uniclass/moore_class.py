dbea98b5420dd080c8efdcf4e5cd059fa777c6a5
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-07-11 08:04:27 +0300

    add primary error catching

--- first_uniclass/moore_class.py
+++ first_uniclass/moore_class.py
@@ -1,12 +1,13 @@
-from typing import Callable
-
-
 class MooreMachine:
+    """
+      Means: 
+        шаблон класса конечного автомата Мура
+    """
     _number: str
     _answer: str = ""
     _states_dict: dict[str, list[str]]
 
-    def __init__(self, number: str, states_dict: dict,
+    def __init__(self, number: str, states_dict: dict[str, list[str]],
                  add_zeros: bool = True, zeros_amount: int = 4, should_start=True) -> None:
         """
         Args:
@@ -53,12 +54,21 @@ class MooreMachine:
 
         try:
             if self._number[-1] == "0" and self._states_dict.get(next_0_state) != None:
+                if self._states_dict.get(next_0_state)[0] == None or \
+                   self._states_dict.get(next_0_state)[1] == None or \
+                   self._states_dict.get(next_0_state)[2] == None:
+                    raise ValueError("Extra value type in states_dict")
+
                 # выполняем функцию, соотв. "0 на входе"
                 self._DoState(self._states_dict.get(next_0_state)[0],
                               self._states_dict.get(next_0_state)[1],
                               self._states_dict.get(next_0_state)[2])
 
             elif self._number[-1] == "1" and self._states_dict.get(next_1_state) != None:
+                if self._states_dict.get(next_1_state)[0] == None or \
+                   self._states_dict.get(next_1_state)[1] == None or \
+                   self._states_dict.get(next_1_state)[2] == None:
+                    raise ValueError("Extra value type in states_dict")
                 # выполняем функцию, соотв. "1 на входе"
                 self._DoState(self._states_dict.get(next_1_state)[0],
                               self._states_dict.get(next_1_state)[1],
@@ -92,6 +102,6 @@ if __name__ == "__main__":
         print()
 
     # bigger testing
-    # for i in range(0, 10000):
-    #     machine = MooreMachine(bin(i)[2::], states_dict)
-    #     assert (bin(i+3)[2::] == machine.GetAnswer())
+    for i in range(0, 10000):
+        machine = MooreMachine(bin(i)[2::], states_dict)
+        assert (bin(i+3)[2::] == machine.GetAnswer())
