df6b0cf9af991cac104c14af888823ac12f6cdf4
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-08-04 02:48:41 +0300

    rewrite moore_machine

--- examples/number_plus_three.py
+++ examples/number_plus_three.py
@@ -7,13 +7,13 @@ def NumberPlusThree() -> None:
   print()
 
   # словарь конечного автомата, который прибавляет 3 (bin: 11) к исходному числу
-  plus_three_states_dict: dict[int, list[int | str]] = {
-    0: [1, 2, ""],
-    1: [3, 4, 1],
-    2: [4, 1, 0],
-    3: [5, 3, 1],
-    4: [3, 4, 0],
-    5: [5, 3, 0],
+  plus_three_states_dict: MooreMachine.Dict = {
+    "S_0": ({0: "S_1", 1: "S_2"}, None),
+    "S_1": ({0: "S_3", 1: "S_4"}, 1),
+    "S_2": ({0: "S_4", 1: "S_1"}, 0),
+    "S_3": ({0: "S_5", 1: "S_3"}, 1),
+    "S_4": ({0: "S_3", 1: "S_4"}, 0),
+    "S_5": ({0: "S_5", 1: "S_3"}, 0),
   }
 
   for i in range(0, 100):
@@ -25,11 +25,15 @@ def NumberPlusThree() -> None:
     real_answer: str = bin(i + 3)[2::]
     print(f"real answer: {real_answer}")
 
-    machine = MooreMachine(bin(i)[2::], plus_three_states_dict)
+    machine = MooreMachine(bin(i)[2::], plus_three_states_dict, "S_0")
     print(f"machine ans: {machine.GetAnswer()}")
     print()
 
   # bigger testing
   for i in range(0, 10000):
-    machine = MooreMachine(bin(i)[2::], plus_three_states_dict)
+    machine = MooreMachine(bin(i)[2::], plus_three_states_dict, "S_0")
     assert bin(i + 3)[2::] == machine.GetAnswer()
+
+
+if __name__ == "__main__":
+  NumberPlusThree()
--- uniclasses/moore_machine.py
+++ uniclasses/moore_machine.py
@@ -1,112 +1,205 @@
+from uniclasses.state import BinaryDigit, MooreState, StateNameType
+
+
 class MooreMachine:
   """
   Means:
-    Шаблон класса конечного автомата Мура
+    Шаблон класса конечного автомата Мура для обработки двоичных чисел.
+    Пример использования:
+    ```python
+      machine = MooreMachine(
+        number="101",
+        states_dict={
+          "S_0": ({0: "S_1", 1: "S_2"}, None),
+          "S_1": ({0: "S_1", 1: "S_2"}, 1),
+          "S_2": ({0: "S_1", 1: "S_2"}, 0),
+        },
+        initial_state="S_0",
+        add_zeros=True,
+        zeros_amount=3,
+        should_start=True,
+      )
+
+      print(machine.GetAnswer())
+    ```
   """
+
+  Dict = dict[
+    StateNameType,
+    tuple[dict[BinaryDigit, StateNameType], BinaryDigit | None],
+  ]
 
   _number: str
   _answer: str = ""
-  _states_dict: dict[int, list[int | str]]
+  _states: dict[StateNameType, MooreState] = {}
+  _initial_state: StateNameType
 
   def __init__(
     self,
     number: str,
-    states_dict: dict[int, list[int | str]],
+    states_dict: Dict,
+    initial_state: StateNameType,
     add_zeros: bool = True,
     zeros_amount: int = 4,
     should_start: bool = True,
   ) -> None:
     """
     Args:
-      number (str): двоичное число, по которому будет проходиться конечный автомат
-      states_dict (dict[int, list[int | str]]): словарь состояний
-      add_zeros (bool, optional):
-        факт необходимости незначащих доп. нулей (defaults to True)
-      zeros_amount (int, optional): кол-во незначащих доп. нулей (defaults to 4)
-      should_start (bool, optional):
-        факт необходимости начала работы автомата (defaults to True)
+        number (str): двоичное число, по которому будет проходиться конечный автомат.
+        states_dict (MooreMachine.Dict): словарь состояний.
+        initial_state (str | int): начальное состояние.
+        add_zeros (bool, optional): факт необходимости незначащих доп. нулей.
+          Defaults to True.
+        zeros_amount (int, optional): кол-во незначащих доп. нулей. Defaults to 4.
+        should_start (bool, optional): факт необходимости начала работы автомата.
+          Defaults to True.
     """
 
-    self._states_dict = states_dict
-    self._CheckStatesDict()
+    self._initial_state = initial_state
+
+    self._states = {}
+    self._InitStates(states_dict)
 
     self._number = number.zfill(len(number) + zeros_amount * add_zeros)
+    self._answer: str = ""
 
     if should_start:
       self.Start()
 
-  def Start(self) -> None:
-    """
-    Does:
-      запускает алгоритм конечного автомата
-    """
-
-    self._DoState(0)
-
   def GetAnswer(self) -> str:
     """
     Returns:
-      str: вывод конечного автомата
+      str: вывод конечного автомата.
     """
 
-    return str(int(self._answer))
+    return self._answer
 
-  def _DoState(self, state_id: int):
-    """
-    Means:
-      Вспомогательная функция, отвечающая за выполнение действий
-      в состояниях конечного автомата
-
-    Args:
-      state_id (int): номер состояния
-    """
-    make_shift: bool = True
-
-    digit = str(self._states_dict.get(state_id)[-1])  # type: ignore
-
-    if digit == "":
-      make_shift = False
-
-    self._answer += digit
-    if make_shift:
-      self._number = self._number[:-1]
-
-    try:
-      next: int = int(
-        self._number[-1]
-      )  # следующее число на вход (последнее в изначальном)
-      self._DoState(self._states_dict.get(state_id)[next])  # type: ignore
-
-    except IndexError:
-      self._answer = "".join(reversed(self._answer))
-
-  def _CheckStatesDict(self) -> None:
+  def Start(self) -> None:
     """
     Does:
-      Проверяет, что словарь `self._states_dict` соответствует структуре
+      Запускает алгоритм конечного автомата.
+    """
+
+    state_name = self._initial_state
+
+    while self._number:
+      state = self._states[state_name]
+
+      last_digit = int(self._number[-1])
+      self._number = self._number[:-1]
+
+      # TODO: добавить логику с остановкой автомата в конкретном состоянии
+
+      self._answer += str(state.Digit()) if state.Digit() is not None else ""
+      state_name = state.StateIf(last_digit)  # type: ignore
+
+    self._answer = str(int("".join(reversed(self._answer))))
+
+  def _InitStates(self, states_dict: Dict):
+    """
+    Does:
+      Заполняет `self._states` и проверяет,
+      что словарь `states_dict` соответствует структуре `MooreMachine.Dict`:
+      ```python
+        example: MooreMachine.Dict = {
+          "S_0": ({0: "S_1", 1: "S_2"}, None),
+          "S_1": ({0: "S_3", 1: "S_4"}, 1),
+          "S_2": ({0: "S_4", 1: "S_1"}, 0),
+          "S_3": ({0: "S_5", 1: "S_3"}, 1),
+          "S_4": ({0: "S_3", 1: "S_4"}, 0),
+          "S_5": ({0: "S_5", 1: "S_3"}, 0),
+        }
+      ```
 
     Raises:
-      ValueError: если не соответствует
+      ValueError | KeyError: если не соответствует.
     """
-    for key, value in self._states_dict.items():
-      if not isinstance(key, int):
-        raise ValueError("states_dict: not all keys are ints")
 
-      if not isinstance(value, list) or len(value) != 3:  # noqa: PLR2004
-        raise ValueError("states_dict: not all states are lists of 3")
+    if not isinstance(states_dict, dict):
+      raise ValueError(
+        "MooreMachine (in states_dict):\n"
+        "Invalid states_dict type: "
+        f"expected MooreMachine.Dict, but got {type(states_dict)}"
+      )
 
-      for item in value:
-        if not isinstance(item, int | str):
-          raise ValueError("states_dict: extra value type in states")
+    if not states_dict:  # empty
+      raise ValueError(
+        "MooreMachine (in states_dict):\nInvalid states_dict: expected non-empty dict"
+      )
 
-        if (
-          isinstance(item, int)
-          and item in value[:-1]
-          and item not in self._states_dict.keys()
-        ):
-          raise ValueError(f"states_dict: extra key {item}")
+    if self._initial_state not in states_dict.keys():
+      raise KeyError(
+        "MooreMachine (in states_dict):\n"
+        f"Invalid initial state '{self._initial_state}' is not defined in states_dict. "
+        f"Available states: {list(states_dict.keys())}"
+      )
 
-        if (isinstance(item, str) and item not in ("1", "0", "")) or (
-          isinstance(item, int) and item == value[-1] and item not in (1, 0)
-        ):
-          raise ValueError("states_dict: the value of state is not binary")
+    for state_name, state_tuple in states_dict.items():
+      if not isinstance(state_name, StateNameType):
+        raise KeyError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid state name '{state_name}': "
+          f"expected str or int, but got {type(state_name)}"
+        )
+
+      if not isinstance(state_tuple, tuple):
+        raise ValueError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid state_dict type in state '{state_name}': "
+          f"expected tuple, but got {type(state_tuple)}"
+        )
+
+      if not state_tuple:  # empty
+        raise ValueError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid state_dict in state '{state_name}': expected non-empty dict"
+        )
+
+      if len(state_tuple) != 2:  # noqa: PLR2004
+        raise ValueError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid state_tuple length in state '{state_name}': "
+          f"expected (dict, 0 or 1), but got {state_tuple}"
+        )
+
+      output_dict, output_digit = state_tuple
+
+      if not isinstance(output_dict, dict):
+        raise ValueError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid transitions type in state '{state_name}': "
+          f"expected dict, but got {type(output_dict)}"
+        )
+
+      if set(output_dict.keys()) != {0, 1}:
+        raise KeyError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid transitions in state '{state_name}': "
+          f"expected keys 0 and 1, but got {list(output_dict.keys())}"
+        )
+
+      for next_digit, next_state in output_dict.items():
+        if not isinstance(next_state, StateNameType):
+          raise ValueError(
+            "MooreMachine (in states_dict):\n"
+            f"Invalid next_state name in state '{state_name}' in transition "
+            f"'{state_name}' --({next_digit})--> '{next_state}', ...: "
+            f"expected tuple, but got {type(next_state)}"
+          )
+
+      if output_digit not in {0, 1, None}:
+        raise ValueError(
+          "MooreMachine (in states_dict):\n"
+          f"Invalid transition in state '{state_name}': "
+          f"'{state_name}' --(...)--> ..., {output_digit}: "
+          f"expected 0, 1 or None, but got {output_digit}"
+        )
+
+      self._states.setdefault(
+        state_name,
+        MooreState(
+          state_tuple[0][0],  # next_state if input=0
+          state_tuple[0][1],  # next_state if input=1
+          output_digit,  # == state_tuple[1]
+        ),
+      )
