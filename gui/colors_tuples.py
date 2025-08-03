fe5bcbc6b0ab376181af935cfa1811f7cdc2e8e2
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-08-03 18:26:52 +0300

    update graphic colors

---
+++ gui/colors_tuples.py
@@ -0,0 +1,7 @@
+Color = tuple[int, int, int, int]
+
+GRAY: Color = (100, 100, 100, 255)
+DARK_GRAY: Color = (70, 70, 70, 255)
+LIGHT_GRAY: Color = (130, 130, 130, 255)
+
+BLUE_GRAY: Color = (124, 173, 187, 255)
